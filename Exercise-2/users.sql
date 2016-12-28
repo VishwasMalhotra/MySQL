df809be6df96970a4e4469fbeb80c64ad6dd17dc
VishwasMalhotra <vishwasmalhotra14@gmail.com>
2016-12-28 11:50:57 +0530

    Corrections

--- Exercise-2/users.sql
+++ Exercise-2/users.sql
@@ -1,4 +1,8 @@
-CREATE DATABASE vtapp;
-CREATE USER 'vtapp_user'@'localhost' IDENTIFIED BY 'password123';
-GRANT ALL PRIVILEGES ON *.* TO 'vtapp_user'@'localhost'
-WITH GRANT OPTION;
+mysql> CREATE DATABASE vtapp;
+Query OK, 1 row affected (0.03 sec)
+
+mysql> CREATE USER 'vtapp_user' IDENTIFIED BY 'password123';
+Query OK, 0 rows affected (0.12 sec)
+
+mysql> GRANT ALL PRIVILEGES ON vtapp.* TO 'vtapp_user';
+Query OK, 0 rows affected (0.08 sec)
