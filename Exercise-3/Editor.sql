2583a9f07aa350218bb72ad4bc824dcfd6e66e39
VishwasMalhotra <vishwasmalhotra14@gmail.com>
2017-01-04 18:24:52 +0530

    Re-Did the whole Exercise.

--- Exercise-3/Editor.sql
+++ Exercise-3/Editor.sql
@@ -1,25 +1,31 @@
-mysql> CREATE DATABASE editor_choice;
-Query OK, 1 row affected (0.00 sec)
+mysql> CREATE DATABASE exercise_3;
+Query OK, 1 row affected (0.04 sec)
 
-mysql> use editor_choice;
+mysql> use exercise_3
 Database changed
 
--- 1. Manage(create, update, delete) categories, articles, comments, and users.
-
+--1. Manage(create, update, delete) categories, articles, comments, and users
 mysql> CREATE TABLE users (
     -> id INT AUTO_INCREMENT,
-    -> user_name VARCHAR(30),
-    -> user_type VARCHAR(10),
+    -> `name` VARCHAR(30),
+    -> type ENUM('admin', 'user'),
     -> PRIMARY KEY(id)
     -> );
-Query OK, 0 rows affected (0.10 sec)
+Query OK, 0 rows affected (0.06 sec)
+
+mysql> CREATE TABLE to_delete (
+    -> id INT,
+    -> test VARCHAR(20),
+    -> PRIMARY KEY(id)
+    -> );
+Query OK, 0 rows affected (0.06 sec)
 
 mysql> CREATE TABLE categories (
     -> id INT AUTO_INCREMENT,
-    -> category VARCHAR(30),
+    -> classification VARCHAR(30),
     -> PRIMARY KEY(id)
     -> );
-Query OK, 0 rows affected (0.09 sec)
+Query OK, 0 rows affected (0.06 sec)
 
 mysql> CREATE TABLE articles (
     -> id INT AUTO_INCREMENT,
@@ -30,20 +36,35 @@ mysql> CREATE TABLE articles (
     -> FOREIGN KEY(user_id) REFERENCES users(id),
     -> FOREIGN KEY(categories_id) REFERENCES categories(id)
     -> );
-Query OK, 0 rows affected (0.15 sec)
+Query OK, 0 rows affected (0.07 sec)
 
 mysql> CREATE TABLE comments (
     -> id INT AUTO_INCREMENT,
-    -> user_comment TEXT,
+    -> `comment` TEXT,
     -> user_id INT,
     -> article_id INT,
     -> PRIMARY KEY(id),
     -> FOREIGN KEY(user_id) REFERENCES users(id),
     -> FOREIGN KEY(article_id) REFERENCES articles(id)
     -> );
-Query OK, 0 rows affected (0.18 sec)
+Query OK, 0 rows affected (0.07 sec)
 
-mysql> INSERT INTO users (user_name, user_type)
+mysql> INSERT INTO to_delete(id,test)
+    -> VALUES (1,'Hello'),
+    -> (2,'Hi');
+Query OK, 2 rows affected (0.00 sec)
+Records: 2  Duplicates: 0  Warnings: 0
+
+mysql> UPDATE to_delete
+    -> SET test = 'Hi, how are you?'
+    -> WHERE id=1;
+Query OK, 1 row affected (0.00 sec)
+Rows matched: 1  Changed: 1  Warnings: 0
+
+mysql> DROP TABLE to_delete;
+Query OK, 0 rows affected (0.02 sec)
+
+mysql> INSERT INTO users (name, type)
     -> VALUES ('Rahul', 'admin'),
     -> ('Abhishek', 'user'),
     -> ('user3', 'user'),
@@ -57,7 +78,7 @@ mysql> INSERT INTO users (user_name, user_type)
 Query OK, 10 rows affected (0.00 sec)
 Records: 10  Duplicates: 0  Warnings: 0
 
-mysql> INSERT INTO categories (category)
+mysql> INSERT INTO categories (classification)
     -> VALUES ('Music'),
     -> ('Sports'),
     -> ('Movies'),
@@ -66,127 +87,149 @@ Query OK, 4 rows affected (0.00 sec)
 Records: 4  Duplicates: 0  Warnings: 0
 
 mysql> INSERT INTO articles (content, user_id, categories_id)
-    -> VALUES ('Hello, This is an article.', 3, 1),
-    -> ('Hello, This is an article.', 1, 2),
-    -> ('Hello, This is an article.', 8, 3),
-    -> ('Hello, This is an article.', 5, 4),
-    -> ('Hello, This is an article.', 6, 4),
-    -> ('Hello, This is an article.', 7, 3),
-    -> ('Hello, This is an article.', 2, 2),
-    -> ('Hello, This is an article.', 3, 1),
-    -> ('Hello, This is an article.', 3, 2),
-    -> ('Hello, This is an article.', 5, 3),
-    -> ('Hello, This is an article.', 7, 4),
-    -> ('Hello, This is an article.', 6, 1),
-    -> ('Hello, This is an article.', 4, 2),
-    -> ('Hello, This is an article.', 9, 2),
-    -> ('Hello, This is an article.', 10, 3),
+    -> VALUES ('John is a good boy.', 3, 1),
+    -> ('John plays tennis.', 1, 2),
+    -> ('He is smart.', 8, 3),
+    -> ('Sherlock lives in london.', 5, 4),
+    -> ('His address is 221B, Baker St.', 6, 4),
+    -> ('Watson is his assistant.', 7, 3),
+    -> ('This is another article.', 2, 2),
+    -> ('Hi, how are you?', 3, 1),
+    -> ('I am good.', 3, 2),
+    -> ('What\'s up?', 5, 3),
+    -> ('Where do you live?', 7, 4),
+    -> ('I stay in Delhi.', 6, 1),
+    -> ('Delhi is in India.', 4, 2),
+    -> ('Global warming.', 9, 2),
+    -> ('Another Article on Global Warming.', 10, 3),
     -> ('Hello, This is an article.', 10, 4),
-    -> ('Hello, This is an article.', 8, 3),
-    -> ('Hello, This is an article.', 9, 4),
-    -> ('Hello, This is an article.', 2, 1),
-    -> ('Hello, This is an article.', 6, 4),
-    -> ('Hello, This is an article.', 5, 1);
+    -> ('Article on X.', 8, 3),
+    -> ('Article on Y.', 9, 4),
+    -> ('Article on ABC.', 2, 1),
+    -> ('Article on XYZ.', 6, 4),
+    -> ('Hi, this is an article.', 5, 1);
 Query OK, 21 rows affected (0.00 sec)
 Records: 21  Duplicates: 0  Warnings: 0
 
-mysql> INSERT INTO comments(user_comment, user_id, article_id)
-    -> VALUES ('Hi, This is comment.', 3, 2),
-    -> ('Hi, This is comment.', 4, 1),
-    -> ('Hi, This is comment.', 5, 8),
-    -> ('Hi, This is comment.', 3, 20),
-    -> ('Hi, This is comment.', 7, 1),
-    -> ('Hi, This is comment.', 8, 3),
-    -> ('Hi, This is comment.', 9, 3),
-    -> ('Hi, This is comment.', 10, 6),
-    -> ('Hi, This is comment.', 1, 10),
-    -> ('Hi, This is comment.', 1, 15);
+mysql> INSERT INTO comments(`comment`, user_id, article_id)
+    -> VALUES ('Good.', 3, 2),
+    -> ('Great.', 4, 1),
+    -> ('Nice.', 5, 8),
+    -> ('Bad.', 3, 20),
+    -> ('Better.', 7, 1),
+    -> ('Worse.', 8, 3),
+    -> ('Worst.', 9, 3),
+    -> ('Perfect.', 10, 6),
+    -> ('Ok.', 1, 10),
+    -> ('Fine.', 1, 15);
 Query OK, 10 rows affected (0.00 sec)
 Records: 10  Duplicates: 0  Warnings: 0
 
--- 2. Select all articles whose author's name is user3 (Do this exercise using variable also).
-mysql> SELECT content, user_name, user_id
+--2 a. Select all articles whose author's name is user3.
+mysql> SELECT content, name, user_id
     -> FROM articles JOIN users
     -> ON articles.user_id = users.id
-    -> WHERE user_name= 'user3';
-+----------------------------+-----------+---------+
-| content                    | user_name | user_id |
-+----------------------------+-----------+---------+
-| Hello, This is an article. | user3     |       3 |
-| Hello, This is an article. | user3     |       3 |
-| Hello, This is an article. | user3     |       3 |
-+----------------------------+-----------+---------+
+    -> WHERE name= 'user3';
++---------------------+-------+---------+
+| content             | name  | user_id |
++---------------------+-------+---------+
+| John is a good boy. | user3 |       3 |
+| Hi, how are you?    | user3 |       3 |
+| I am good.          | user3 |       3 |
++---------------------+-------+---------+
+3 rows in set (0.00 sec)
+
+--2 b. Select all articles whose author's name is user3 using variable.
+mysql> SET @thirdUser = 'user3';
+Query OK, 0 rows affected (0.00 sec)
+
+mysql> SELECT content, name, user_id
+    -> FROM articles JOIN users
+    -> ON articles.user_id = users.id
+    -> WHERE name = @thirdUser;
++---------------------+-------+---------+
+| content             | name  | user_id |
++---------------------+-------+---------+
+| John is a good boy. | user3 |       3 |
+| Hi, how are you?    | user3 |       3 |
+| I am good.          | user3 |       3 |
++---------------------+-------+---------+
 3 rows in set (0.00 sec)
 
 --3. For all the articles being selected above, select all the articles and also the comments associated with those articles in a single query.
-mysql> SELECT content, user_comment AS 'Comments by User'
-    -> FROM articles LEFT JOIN comments
-    -> ON articles.id = comments.article_id
-    -> WHERE articles.user_id = 3;
-+----------------------------+----------------------+
-| content                    | Comments by User     |
-+----------------------------+----------------------+
-| Hello, This is an article. | Hi, This is comment. |
-| Hello, This is an article. | Hi, This is comment. |
-| Hello, This is an article. | Hi, This is comment. |
-| Hello, This is an article. | NULL                 |
-+----------------------------+----------------------+
+mysql> SELECT content, comment AS 'Comments by User'
+    -> FROM articles JOIN users
+    -> ON articles.user_id=users.id LEFT JOIN comments
+    -> ON comments.article_id=articles.id
+    -> WHERE users.name='user3';
++---------------------+------------------+
+| content             | Comments by User |
++---------------------+------------------+
+| John is a good boy. | Great.           |
+| John is a good boy. | Better.          |
+| Hi, how are you?    | Nice.            |
+| I am good.          | NULL             |
++---------------------+------------------+
 4 rows in set (0.00 sec)
 
 
--- 3. Using subquery.
-mysql> SELECT content, user_comment AS 'Comments by User'
+-- 3 b. Using subquery.
+mysql> SELECT content, comment AS 'Comments by User'
     -> FROM articles LEFT JOIN comments
     -> ON articles.id = comments.article_id
-    -> WHERE articles.user_id = (SELECT id FROM users WHERE user_name = 'user3');
-+----------------------------+----------------------+
-| content                    | Comments by User     |
-+----------------------------+----------------------+
-| Hello, This is an article. | Hi, This is comment. |
-| Hello, This is an article. | Hi, This is comment. |
-| Hello, This is an article. | Hi, This is comment. |
-| Hello, This is an article. | NULL                 |
-+----------------------------+----------------------+
+    -> WHERE articles.user_id = (SELECT id FROM users WHERE name = 'user3');
++---------------------+------------------+
+| content             | Comments by User |
++---------------------+------------------+
+| John is a good boy. | Great.           |
+| John is a good boy. | Better.          |
+| Hi, how are you?    | Nice.            |
+| I am good.          | NULL             |
++---------------------+------------------+
 4 rows in set (0.00 sec)
 
 --4. Write a query to select all articles which do not have any comments.
 mysql> SELECT content
     -> FROM articles LEFT JOIN comments
     -> ON articles.id = comments.article_id
-    -> WHERE user_comment IS NULL;
-+----------------------------+
-| content                    |
-+----------------------------+
-| Hello, This is an article. |
-| Hello, This is an article. |
-| Hello, This is an article. |
-| Hello, This is an article. |
-| Hello, This is an article. |
-| Hello, This is an article. |
-| Hello, This is an article. |
-| Hello, This is an article. |
-| Hello, This is an article. |
-| Hello, This is an article. |
-| Hello, This is an article. |
-| Hello, This is an article. |
-| Hello, This is an article. |
-+----------------------------+
+    -> WHERE comment IS NULL;
++--------------------------------+
+| content                        |
++--------------------------------+
+| Sherlock lives in london.      |
+| His address is 221B, Baker St. |
+| This is another article.       |
+| I am good.                     |
+| Where do you live?             |
+| I stay in Delhi.               |
+| Delhi is in India.             |
+| Global warming.                |
+| Hello, This is an article.     |
+| Article on X.                  |
+| Article on Y.                  |
+| Article on ABC.                |
+| Hi, this is an article.        |
++--------------------------------+
 13 rows in set (0.00 sec)
 
 -- 5. Write a query to select article which has maximum comments.
 mysql> SELECT articles.id, content FROM articles
-    -> JOIN comments ON articles.id=comments.article_id GROUP BY articles.id HAVING COUNT(comments.id) =
+    -> JOIN comments ON articles.id=comments.article_id
+    -> GROUP BY articles.id
+    -> HAVING COUNT(comments.id)
+    -> =
     -> (
-    -> SELECT MAX(comment_count) FROM (
-    -> SELECT article_id, COUNT(article_id) as comment_count FROM comments GROUP BY article_id
-    -> ) AS X
+    -> SELECT DISTINCT COUNT(article_id) AS comment_count
+    -> FROM comments
+    -> GROUP BY article_id
+    -> LIMIT 1
     -> );
-+----+----------------------------+
-| id | content                    |
-+----+----------------------------+
-|  1 | Hello, This is an article. |
-|  3 | Hello, This is an article. |
-+----+----------------------------+
++----+---------------------+
+| id | content             |
++----+---------------------+
+|  1 | John is a good boy. |
+|  3 | He is smart.        |
++----+---------------------+
 2 rows in set (0.00 sec)
 
 --6. Write a query to select article which does not have more than one comment by the same user.
@@ -195,16 +238,17 @@ mysql> SELECT articles.id, content
     -> ON articles.id=comments.article_id
     -> GROUP BY articles.id
     -> HAVING COUNT(comments.user_id) = COUNT(DISTINCT comments.user_id);
-+----+----------------------------+
-| id | content                    |
-+----+----------------------------+
-|  1 | Hello, This is an article. |
-|  2 | Hello, This is an article. |
-|  3 | Hello, This is an article. |
-|  6 | Hello, This is an article. |
-|  8 | Hello, This is an article. |
-| 10 | Hello, This is an article. |
-| 15 | Hello, This is an article. |
-| 20 | Hello, This is an article. |
-+----+----------------------------+
-8 rows in set (0.00 sec)
++----+------------------------------------+
+| id | content                            |
++----+------------------------------------+
+|  1 | John is a good boy.                |
+|  2 | John plays tennis.                 |
+|  3 | He is smart.                       |
+|  6 | Watson is his assistant.           |
+|  8 | Hi, how are you?                   |
+| 10 | What's up?                         |
+| 15 | Another Article on Global Warming. |
+| 20 | Article on XYZ.                    |
++----+------------------------------------+
+8 rows in set (0.05 sec)
+
